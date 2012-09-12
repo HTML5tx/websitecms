4bdebf143aeaa1dd8fc343fc0ff1e8e2863c2142
Brandon Satrom <bsatrom+gh@gmail.com>
2012-09-12 08:01:24 -0500

    finalized layout

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,7 @@ Websitecms::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = false
+  config.assets.debug = true
 
   # from the active-admin settings
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
