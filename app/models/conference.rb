373287f8e608ede170855285ad9bf4f39b443fbf
Tim G. Thomas <tim@timgthomas.com>
2012-12-09 18:08:45 -0600

    remove some models that rely on activerecord

--- app/models/conference.rb
+++
@@ -1,4 +0,0 @@
-class Conference < ActiveRecord::Base
-  attr_accessible :date, :id, :location_address, :location_map_url, :name, :venue_description
-  has_many :talks
-end
--- app/models/person.rb
+++
@@ -1,5 +0,0 @@
-class Person < ActiveRecord::Base
-  attr_accessible :bio, :facebook, :github, :home_city, :homepage, :id, :name, :twitter
-
-  has_many :talks
-end
